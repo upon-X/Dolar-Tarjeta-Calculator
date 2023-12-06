6a4fde18b3c43ced7a8f49d0bcb6b72cc0df574a
Miche <valemiche003@gmail.com>
2023-12-06 06:49:29 -0300

    añadi un correo para reportar fallos, y aparte la funcion de la fecha de act del precio del tipo de cambio

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,35 @@ import axios from 'axios';
 export default function App() {
   const [precioDolares, setPrecioDolares] = useState(1);
   const [dolarOficial, setDolarOficial] = useState(null);
+  const [fechaAct, setFechaAct] = useState('')
   const [precioPesos, setPrecioPesos] = useState(null);
   const [impuestoPais, setImpuestoPais] = useState(null);
   const [retencion, setRetencion] = useState(null);
   const [bienesPersonales, setBienesPersonales] = useState(null);
   const [precioCalculado, setPrecioCalculado] = useState(null);
+  const [widthPage, setWidthPage] = useState(true);
 
+  const mailcel = 'mailto:valemiche003e@gmail.com.ar?subject=CalculadoraDolarTarjeta'
+  const mailpc = 'https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=valemiche003@gmail.com&su=CalculadoraDolarTarjeta'
+
+  const formatDate = (dateString) => {
+    const options = {
+      day: 'numeric',
+      month: 'numeric',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: false,
+    };
+
+    const date = new Date(dateString);
+    return new Intl.DateTimeFormat('es-ES', options).format(date);
+  };
   const fetchData = () => {
     axios.get('https://dolarapi.com/v1/dolares/oficial')
       .then(response => {
         setDolarOficial(response.data.venta);
+        setFechaAct(formatDate(response.data.fechaActualizacion));
       })
       .catch(error => {
         // Manejo de errores
@@ -25,6 +44,11 @@ export default function App() {
   // Efecto para cargar datos al montar el componente y establecer el valor inicial de precioDolares
   useEffect(() => {
     fetchData();
+    if (window.innerWidth > 700) {
+      setWidthPage(true)
+    } else {
+      setWidthPage(false)
+    }
   }, []);
 
   const handleFormSubmit = (event) => {
@@ -59,14 +83,19 @@ export default function App() {
         <button className='btn_calculate' type="submit" disabled={precioDolares === 0 || !precioDolares ? true : false}>Calcular</button>
       </form>
       <div className='info_precio'>
+        <span>Precio Dolar Oficial (Venta): <span className='resultados'>${dolarOficial || '0'}</span></span>
+        <span className='fecha_actualizacion'>Ultima actualización: <span className='resultados'>{fechaAct || '0'}</span></span>
         <span>Precio en pesos (Dolar Oficial): <span className='resultados'>${precioPesos || '0'}</span></span>
         <span>Impuesto País 30%: <span className='resultados'>${impuestoPais || '0'}</span></span>
         <span>Retención del 100%: <span className='resultados'>${retencion || '0'}</span></span>
         <span>Bienes Personales 25%: <span className='resultados'>${bienesPersonales || '0'}</span></span>
         <span>Precio Total en pesos: <span className='resultados resultado_final'>${precioCalculado || '0'}</span></span>
       </div>
-      <p>Esto nos sirve para saber cuanto nos costara alguna compra en dolares, como en Steam, en Amazon, etc.</p>
+      <p>Esto nos sirve para saber cuanto nos costara alguna compra en dolares, como en Steam, en Amazon, etc.
+        <br />
+        Cualquier duda o problema, contactar a este <a target='_blank' href={widthPage === true ? mailpc : mailcel}>Mail</a>
+      </p>
       <p className='link_portfolio'>Coded by <a href='https://valemiche.com.ar'>Valentino Micheloni</a></p>
-    </div>
+    </div >
   );
 }
